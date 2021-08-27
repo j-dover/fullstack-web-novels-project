6c4ec3946b5937cdeb0355c9933357933604711e
j-dover <aldover27@gmail.com>
2021-08-27 00:22:16 -0400

    add user account controller file

---
+++ server/controllers/UserAccountController.js
@@ -0,0 +1,16 @@
+var User = require('../models/UserAccountModel');
+
+/**
+ * Interacts with the model to obtain public userAccountData
+ * and sends a JSON response
+ * @param {object} req Object containing HTTP request data
+ * @param {object} res Object containing HTTP response data
+ */
+
+
+/**
+ * Interacts with the model to insert a new user account into the database
+ * @param {object} req Object containing HTTP request data
+ * @param {object} res Object containing HTTP response data
+ */
+
